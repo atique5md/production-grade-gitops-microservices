9a9465d77fe962d4063dfc6dc86be7fb86d0c831
atique <atiquem550@gmail.com>
2026-09-06 14:27:25 +0530

    Add Terraform VPC networking

---
+++ terraform/variables.tf
@@ -0,0 +1,28 @@
+variable "aws_region" {
+  description = "AWS region where the infrastructure will be created"
+  type        = string
+  default     = "ap-south-1"
+}
+
+variable "project_name" {
+  description = "Name used for AWS resources"
+  type        = string
+  default     = "production-grade-gitops"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the project VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+variable "public_subnet_cidrs" {
+  description = "CIDR blocks for public subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  description = "CIDR blocks for private subnets"
+  type        = list(string)
+  default     = ["10.0.11.0/24", "10.0.12.0/24"]
+}
